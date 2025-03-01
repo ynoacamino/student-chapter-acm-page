0530ce5aba39e0325990a30433c854d5de621cb3
gustadev24 <lgsc21211@gmail.com>
2025-03-01 09:56:22 -0500

    Add Google Logo and links

--- frontend/src/components/layout/footer/GoogleInfo.tsx
+++ frontend/src/components/layout/footer/GoogleInfo.tsx
@@ -1,16 +1,23 @@
+import GoogleWordLogo from '@/components/logos/GoogleWordLogo';
+import Link from 'next/link';
+
 const googleInfo = [
-  { name: 'About Google', url: '#' },
-  { name: 'Google products', url: '#' },
-  { name: 'Products', url: '#' },
-  { name: 'Terms', url: '#' },
+  { name: 'Acerca de Google', url: 'https://about.google' },
+  { name: 'Productos de Google', url: 'https://about.google/products' },
+  { name: 'Privacidad', url: 'https://policies.google.com/privacy' },
+  { name: 'Términos', url: 'https://policies.google.com/terms' },
+  { name: 'Ayuda', url: 'https://support.google.com/?hl=es' },
 ];
 
 export default function GoogleInfo() {
   return (
-    <div className="flex gap-4 text-muted-foreground">
+    <div className="flex gap-7 text-muted-foreground text-sm flex-wrap">
+      <Link href="https://google.com">
+        <GoogleWordLogo className="fill-muted-foreground" />
+      </Link>
       {
         googleInfo.map(({ name, url }) => (
-          <a key={name} href={url}>{name}</a>
+          <Link key={name} href={url}>{name}</Link>
         ))
       }
     </div>
