6c6ff7be073c9e9d9f9a5934632841131b609ee5
gustadev24 <lgsc21211@gmail.com>
2025-03-04 10:59:47 -0500

    Add temporal types for comites

---
+++ frontend/src/types/comite.ts
@@ -0,0 +1,52 @@
+import { COMITES_ROUTES } from '@/lib/mock'
+
+export interface BaseMember {
+  name: string;
+  img: string;
+  role: 'member' | 'directive' | 'volunteer';
+  links: {
+    github?: string;
+    linkedin?: string;
+    instagram?: string;
+  }
+}
+
+export interface Member extends BaseMember {
+  role: 'member';
+}
+
+export interface Directive extends BaseMember {
+  role: 'directive';
+  position: string;
+}
+
+export interface Volunteer extends BaseMember {
+  role: 'volunteer';
+}
+
+export interface Event {
+  date: string;
+  name: string;
+  description: {
+    short: string;
+    large: string;
+  }
+}
+
+export type ComiteMember = Member | Directive | Volunteer;
+
+export type ComiteRoute = typeof COMITES_ROUTES[number];
+
+export interface Comite {
+  route: ComiteRoute;
+  title: string;
+  description: string;
+  img: string;
+  sections: Array<{ title: string, description: Array<string> }>;
+  members: Array<ComiteMember>;
+  events: {
+    past: Array<Event>
+    future: Array<Event>
+  },
+
+}
