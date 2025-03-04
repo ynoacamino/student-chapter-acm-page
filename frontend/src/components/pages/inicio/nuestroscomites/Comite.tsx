97936b423b214263a334db812353e42ee08d231e
gustadev24 <lgsc21211@gmail.com>
2025-03-04 10:16:38 -0500

    Add come animation to bento cards

--- frontend/src/components/pages/inicio/nuestroscomites/Comite.tsx
+++ frontend/src/components/pages/inicio/nuestroscomites/Comite.tsx
@@ -14,7 +14,7 @@ export default function Comite({
   return (
     <Link
       href={`/comites/${path}`}
-      className={cn('flex flex-col gap-5 border rounded-md border-primary p-5 min-h-72', className)}
+      className={cn('flex flex-col gap-5 border rounded-md border-primary p-5 min-h-72 transition-transform hover:scale-105 ', className)}
     >
       <h3 className="text-3xl font-semibold">
         {title}
