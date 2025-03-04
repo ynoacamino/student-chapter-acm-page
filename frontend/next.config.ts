7ee8f495d1cba662da5be48e7ac5b21fbac57513
gustadev24 <lgsc21211@gmail.com>
2025-03-03 21:16:06 -0500

    Add dynamic route for comites section with not found page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   reactStrictMode: false,
   images: {
     unoptimized: true,
