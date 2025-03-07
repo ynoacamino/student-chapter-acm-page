36f416f77252557fb6a2ea7a44e5a4804f59e1cb
gustadev24 <lgsc21211@gmail.com>
2025-03-06 21:25:30 -0500

    Create function for convert a name to route

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function convertToRoute(route: string) {
+  return route.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().replaceAll(' ', '-');
+}
