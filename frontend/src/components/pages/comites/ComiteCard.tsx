a0114b370f3b9fe35df887a5adfe0842d2aed89f
gustadev24 <lgsc21211@gmail.com>
2025-03-07 21:51:44 -0500

    Create hover card animation

--- frontend/src/components/pages/comites/ComiteCard.tsx
+++ frontend/src/components/pages/comites/ComiteCard.tsx
@@ -1,21 +1,29 @@
+'use client';
+
 import { getThumbnailUrl, slugify } from '@/lib/utils';
 import { Committee } from '@/types/committees';
+import { ExternalLink } from 'lucide-react';
 import Link from 'next/link';
 
 export default function ComiteCard(committee: Committee) {
   const { name, excerpt } = committee;
   return (
-    <div className="group p-[1px] transition-colors rounded-xl bg-transparent hover:bg-primary/30 animate-in">
+    <div className="group p-0.5 rounded-xl relative overflow-hidden h-full">
+      <div className="absolute top-0 left-0 size-full scale-0 bg-primary/50 group-hover:scale-140 transition-transform duration-600 origin-top-left rounded-br-full" />
+      <div className="absolute top-0 left-0 size-full bg-muted -z-10" />
       <Link
         href={`/comites/${slugify(name)}`}
-        className="flex flex-col gap-6 border-muted border-2 rounded-xl p-6 group-hover:border-primary/30 transition-colors bg-background"
+        className="flex flex-col gap-6 rounded-xl transition-colors bg-background overflow-hidden relative h-full min-h-96 z-10"
       >
-        <img src={getThumbnailUrl(committee)} alt={name} className="w-full rounded-md" />
-        <div className="flex flex-col gap-3 h-40 overflow-y-hidden">
+        <ExternalLink className="absolute right-4 top-4 scale-125 -translate-y-[150%] translate-x-[150%] group-hover:translate-0 group-hover:scale-100 transition-transform duration-400" />
+        <div className="h-60 overflow-hidden group-hover:scale-[270%] group-hover:opacity-40 transition-all duration-400 ease-in-out relative">
+          <img src={getThumbnailUrl(committee)} alt={name} className="rounded-t-md object-cover" />
+        </div>
+        <div className="flex flex-col gap-3 px-6 pb-6  z-10">
           <h2 className="text-xl font-semibold">
             {name}
           </h2>
-          <p className="">
+          <p className="grow">
             {excerpt.length > 165 ? `${excerpt.slice(0, 135)}...` : excerpt}
           </p>
         </div>
--- frontend/src/components/pages/comites/ShowComites.tsx
+++ frontend/src/components/pages/comites/ShowComites.tsx
@@ -6,7 +6,7 @@ export default async function ShowComites() {
   const committees = await api.getCommittees();
 
   return (
-    <Section className="grid grid-rows-none grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6 items-start h-full mt-10">
+    <Section className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6 items-start mt-10">
       {
         committees.map((committee) => (
           <ComiteCard
