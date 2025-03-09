d3aa712a4236c448d87c5d7c2aba2d2cc7ca1308
ynoacamino <ynoacamino@gmail.com>
2025-03-09 15:33:59 -0500

    add metadata in static and dinamic pages

--- frontend/src/app/comites/[committeeSlug]/page.tsx
+++ frontend/src/app/comites/[committeeSlug]/page.tsx
@@ -8,6 +8,7 @@ import { notFound } from 'next/navigation';
 import api from '@/lib/api';
 import { getThumbnailUrl, slugify } from '@/lib/utils';
 import { CommitteesFields } from '@/types/committees';
+import { Metadata } from 'next';
 
 export const dynamicParams = false;
 
@@ -17,6 +18,32 @@ export async function generateStaticParams() {
   return committees.map(({ name }) => ({
     committeeSlug: slugify(name),
   }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ committeeSlug: string }>
+}): Promise<Metadata> {
+  const { committeeSlug } = await params;
+  if (!committeeSlug) {
+    notFound();
+  }
+
+  const committee = await api.getCommitteeBySlug({ slug: committeeSlug });
+
+  return {
+    title: `${committee[CommitteesFields.NAME].slice(10)} | ACM UNSA`,
+    openGraph: {
+      images: [
+        {
+          url: getThumbnailUrl(committee),
+          alt: committee[CommitteesFields.NAME],
+        },
+      ],
+    },
+    description: committee[CommitteesFields.EXCERPT],
+  };
 }
 
 export default async function Page({
